d2e3afc8aec45db495e5b9ddb60ff2460d31b39e
GabsTheCuriousKid <140965985+GabsTheCuriousKid@users.noreply.github.com>
2023-08-25 16:01:21 +0200

    Update _manifest_entry.js

--- src/addons/addons/editor-extra-keys/_manifest_entry.js
+++ src/addons/addons/editor-extra-keys/_manifest_entry.js
@@ -15,39 +15,30 @@ const manifest = {
   "info": [
     {
       "type": "notice",
-      "text": "The \"experimental keys\" include equals, slash, semicolon and more. They may not work on all operating systems or keyboard layouts.",
+      "text": "all keys include equals, slash, semicolon and more. They may not work on all operating systems or keyboard layouts.",
       "id": "experimentalKeysWarn"
     },
     {
       "type": "notice",
-      "text": "The \"Shift keys\" include keys which typically require the Shift key and a number key, like hashtag, exclamation mark and more. These keys only work with the \"when () key pressed\" block and do not work on all operating systems or keyboard layouts.",
+      "text": "some keys include keys which typically require the Shift key and a number key, like hashtag, exclamation mark and more. These keys only work with the \"when () key pressed\" block and do not work on all operating systems or keyboard layouts.",
       "id": "shiftKeysWarn"
     },
     {
       "type": "warning",
-      "text": "The \"TurboWarp keys\" will only work in TurboWarp, not in Scratch.",
+      "text": "some keys with words will only work in TurboWarp, not in Scratch.",
+      "id": "twKeysWarn"
+    },
+    {
+      "type": "warning",
+      "text": "german keys like, ä, ö, ü and ß require german computers.",
       "id": "twKeysWarn"
     }
   ],
   "settings": [
     {
       "dynamic": true,
-      "name": "Show experimental keys",
+      "name": "Show all keys",
       "id": "experimentalKeys",
-      "type": "boolean",
-      "default": false
-    },
-    {
-      "dynamic": true,
-      "name": "Show Shift keys",
-      "id": "shiftKeys",
-      "type": "boolean",
-      "default": false
-    },
-    {
-      "dynamic": true,
-      "name": "Show TurboWarp keys",
-      "id": "twKeys",
       "type": "boolean",
       "default": false
     }
