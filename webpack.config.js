0b8f422e8ea5645e402ea6be59b02a8762c9e4ba
MerrCraft <161960686+MerrCraft@users.noreply.github.com>
2024-11-26 20:42:11 -0600

    Names

--- webpack.config.js
+++ webpack.config.js
@@ -174,28 +174,28 @@ module.exports = [
                 chunks: ['playground'],
                 template: 'src/playground/index.ejs',
                 filename: 'playground.html',
-                title: 'PenguinMod - Playground',
+                title: 'MerrIDE - Playground',
                 ...htmlWebpackPluginCommon
             }),
             new HtmlWebpackPlugin({
                 chunks: ['player'],
                 template: 'src/playground/index.ejs',
                 filename: 'index.html',
-                title: 'PenguinMod - A mod of TurboWarp',
+                title: 'MerrIDE - A mod of PenguinMod',
                 ...htmlWebpackPluginCommon
             }),
             new HtmlWebpackPlugin({
                 chunks: ['fullscreen'],
                 template: 'src/playground/index.ejs',
                 filename: 'fullscreen.html',
-                title: 'PenguinMod - A mod of TurboWarp',
+                title: 'MerrIDE - A mod of PenguinMod',
                 ...htmlWebpackPluginCommon
             }),
             new HtmlWebpackPlugin({
                 chunks: ['embed'],
                 template: 'src/playground/index.ejs',
                 filename: 'embed.html',
-                title: 'Embedded Project - PenguinMod',
+                title: 'Embedded Project - MerrIDE',
                 noTheme: true,
                 ...htmlWebpackPluginCommon
             }),
@@ -203,14 +203,14 @@ module.exports = [
                 chunks: ['addon-settings'],
                 template: 'src/playground/simple.ejs',
                 filename: 'addons.html',
-                title: 'Addon Settings - PenguinMod',
+                title: 'Addon Settings - MerrIDE',
                 ...htmlWebpackPluginCommon
             }),
             new HtmlWebpackPlugin({
                 chunks: ['credits'],
                 template: 'src/playground/simple.ejs',
                 filename: 'credits.html',
-                title: 'PenguinMod & TurboWarp Credits',
+                title: 'MerrIDE, PenguinMod & TurboWarp Credits',
                 noSplash: true,
                 ...htmlWebpackPluginCommon
             }),
